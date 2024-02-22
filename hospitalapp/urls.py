37f205dbc89c329163488dcc79164c89874d949f
zawadi-beep <zawadiavery@gmail.com>
2024-02-22 13:11:08 +0300

    firstcommit

---
+++ hospitalapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from hospitalapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index,name = 'index'),
+    path('innerpage/', views.inner,name = 'inner'),
+
+]
